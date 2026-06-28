9dfedf9141fc69a6adf2cef80ae6e15e82791261
oppo6767 <iannmv345@gmail.com>
2026-06-28 13:43:32 +0900

    feat: add pet_state.py

---
+++ pet_state.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Condition(Enum):
+    START = ("start", False)
+    IDLE = ("idle", True)
+    MOVE_LEFT = ("left", True)
+    MOVE_RIGHT = ("right", True)
+    RELAX_1 = ("relax_1", False)
+    RELAX_1_LOOP = ("relax_1_loop", True)
+    RELAX_1_END = ("relax_1_end", False)
+    RELAX_2 = ("relax_2", False)
+    RELAX_2_LOOP = ("relax_2_loop", True)
+    RELAX_2_END = ("relax_2_end", False)
+    STOP = ("stop", False)
+
+    def __init__(self, sheet_key: str, loops: bool):
+        self.sheet_key = sheet_key
+        self.loops = loops
